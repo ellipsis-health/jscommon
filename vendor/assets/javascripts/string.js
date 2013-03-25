6e9141ac1ff644a030684c880070721540cf9bc2
Dieterich Lawson <dieterich.lawson@gmail.com>
2013-03-25 11:52:21 -0500

    Added isString

--- vendor/assets/javascripts/string.js
+++ vendor/assets/javascripts/string.js
@@ -1,3 +1,6 @@
 function upcaseFirstLetter(string){  
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+function isString(obj){
+  return (typeof obj  == 'string' || obj instanceof String);
+}
